8e218a8aeba91d6312352ad1c4cc037c3b1f43bd
hassan_es-sebaiy <hassansobai6@gmail.com>
2026-03-10 14:55:18 +0100

    container

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import {creatRoot} from 'react-dom/client'
+import Main from './main'
 const root=creatRoot(document.getElementById('root'))
 
 root.render(
-    
+   <Main></Main>
 )
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+export default function main(){
+    return(
+       <div className="main">
+          <form action="">
+            <input type="text" placeholder="task" />
+             <button>submet</button>
+          </form>
+       </div>
+    )
+}
