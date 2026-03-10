c849d50b831835dec4a1aece0e9507080efd11da
hassan_es-sebaiy <hassansobai6@gmail.com>
2026-03-10 15:00:27 +0100

    styling form

---
+++ my-app/src/index.jsx
@@ -0,0 +1,8 @@
+import {createRoot} from 'react-dom/client'
+import Main from './main'
+import './style.css'
+const root=createRoot(document.getElementById('root'))
+
+root.render(
+   <Main></Main>
+)
--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import {creatRoot} from 'react-dom/client'
-import Main from './main'
-const root=creatRoot(document.getElementById('root'))
-
-root.render(
-   <Main></Main>
-)
